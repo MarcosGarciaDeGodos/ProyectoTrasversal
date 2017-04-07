d8e4314a3830b2ced2c0557ab18c0f334aeb298d
mgarciadegodos <mgarciadegodos@egibide.org>
2017-04-07 10:47:10 +0200

    Segundo cambio Java

--- Proyecto Java/PruebaGithub/src/pruebagithub/PruebaGithub.java
+++ Proyecto Java/PruebaGithub/src/pruebagithub/PruebaGithub.java
@@ -13,7 +13,7 @@ public class PruebaGithub {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Hola Mundo!!!");
+        System.out.println("Hello World!!!");
     }
     
 }
